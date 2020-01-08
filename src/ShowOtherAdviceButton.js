b00ea6e35a84b0d4033da1c8ae3b7d7933f8a7b7
Jess <jesszeroual@gmail.com>
2020-01-08 16:27:17 +0100

    snapshot-testing

---
+++ src/ShowOtherAdviceButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ShowOtherAdviceButton = ({ loadAdvice }) => (
+  <button className="Advice-other-advice-button" onClick={loadAdvice}>
+    Show other advice
+  </button>
+);
